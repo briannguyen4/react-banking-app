41839a14d5745fc3d1fbc3bc09a0a1b6b9dbb691
Brian Nguyen <brian4nguyen@gmail.com>
2024-03-20 17:17:42 -0400

    update app

--- src/contexts/BankingContext.tsx
+++ src/contexts/BankingContext.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { createContext, useContext, useState } from "react";
 
 const BankingContext = createContext({});
@@ -6,48 +5,48 @@ const BankingContext = createContext({});
 export const useBankingContext = () => useContext(BankingContext);
 
 export const BankingProvider = ({ children }: React.PropsWithChildren<{}>) => {
-    const [checkingTransactions, setCheckingTransactions] = useState([
-        { type: "deposit", amount: 100 },
-        { type: "withdrawal", amount: 20 },
-    ]);
+  const [checkingTransactions, setCheckingTransactions] = useState([
+    { type: "deposit", amount: 100 },
+    { type: "withdrawal", amount: 20 },
+  ]);
 
-    const [checkingBalance, setCheckingBalance] = useState(80);
+  const [checkingBalance, setCheckingBalance] = useState(80);
 
-    const [savingsTransactions, setSavingsTransactions] = useState([
-        { type: "deposit", amount: 1000 },
-        { type: "withdrawal", amount: 200 },
-    ]);
+  const [savingsTransactions, setSavingsTransactions] = useState([
+    { type: "deposit", amount: 1000 },
+    { type: "withdrawal", amount: 200 },
+  ]);
 
-    const [savingsBalance, setSavingsBalance] = useState(800);
+  const [savingsBalance, setSavingsBalance] = useState(800);
 
-    const addCheckingTransaction = (transaction: {
-        type: string;
-        amount: number;
-    }) => {
-        setCheckingTransactions([...checkingTransactions, transaction]);
-    };
+  const addCheckingTransaction = (transaction: {
+    type: string;
+    amount: number;
+  }) => {
+    setCheckingTransactions([...checkingTransactions, transaction]);
+  };
 
-    const addSavingsTransaction = (transaction: {
-        type: string;
-        amount: number;
-    }) => {
-        setSavingsTransactions([...savingsTransactions, transaction]);
-    };
+  const addSavingsTransaction = (transaction: {
+    type: string;
+    amount: number;
+  }) => {
+    setSavingsTransactions([...savingsTransactions, transaction]);
+  };
 
-    return (
-        <BankingContext.Provider
-            value={{
-                checkingTransactions,
-                addCheckingTransaction,
-                checkingBalance,
-                setCheckingBalance,
-                savingsTransactions,
-                addSavingsTransaction,
-                savingsBalance,
-                setSavingsBalance,
-            }}
-        >
-            {children}
-        </BankingContext.Provider>
-    );
+  return (
+    <BankingContext.Provider
+      value={{
+        checkingTransactions,
+        addCheckingTransaction,
+        checkingBalance,
+        setCheckingBalance,
+        savingsTransactions,
+        addSavingsTransaction,
+        savingsBalance,
+        setSavingsBalance,
+      }}
+    >
+      {children}
+    </BankingContext.Provider>
+  );
 };
